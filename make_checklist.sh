91f2e9cc25116c4acdc4647a50b622848f0312b6
Fabien HIEGEL <fabien.hiegel@gmail.com>
2020-01-12 15:17:37 +0100

    French Translation: Add parameter for language in 'make_(checklist|pdf).sh' scripts

--- make_checklist.sh
+++ make_checklist.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-lang=French
-cat "MobProgrammingFacilitorsGuide_${lang}.md" \
+
+POSITIONAL=()
+LANGUAGE="English"
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -l|--language)
+    LANGUAGE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+cat "MobProgrammingFacilitorsGuide_${LANGUAGE}.md" \
        | grep -e '^ *\* \[ \]' -e '^## ' \
-       > "MobProgrammingFacilitorsGuide_${lang}.checklist.md"
+       > "MobProgrammingFacilitorsGuide_${LANGUAGE}.checklist.md"
--- make_pdf.sh
+++ make_pdf.sh
@@ -3,6 +3,24 @@
 # https://www.npmjs.com/package/md-to-pdf
 # npm i -g md-to-pdf
 
-lang=French
-md-to-pdf "MobProgrammingFacilitorsGuide_${lang}.checklist.md"
-md-to-pdf "MobProgrammingFacilitorsGuide_${lang}.md"
+POSITIONAL=()
+LANGUAGE="English"
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -l|--language)
+    LANGUAGE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+md-to-pdf "MobProgrammingFacilitorsGuide_${LANGUAGE}.checklist.md"
+md-to-pdf "MobProgrammingFacilitorsGuide_${LANGUAGE}.md"
